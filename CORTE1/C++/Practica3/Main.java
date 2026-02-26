184351d2c9eccf2f1485e716327a3e40c152aec3
c3r0tr35 <136899104+c3r0tr35@users.noreply.github.com>
2026-02-26 16:27:38 -0500

    Add files via upload

---
+++ CORTE1/C++/Practica3/Main.java
@@ -0,0 +1,24 @@
+class Rectangulo {
+    public double base;
+    public double altura;
+
+    // Constructor
+    public Rectangulo(double b, double h) {
+        base = b;
+        altura = h;
+    }
+
+    public double area() {
+        return base * altura;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangulo r1 = new Rectangulo(3.0, 2.0);
+
+        System.out.println("Base: " + r1.base);
+        System.out.println("Altura: " + r1.altura);
+        System.out.println("Area: " + r1.area());
+    }
+}
